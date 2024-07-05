ad4d077c407b9914ce47f86c9c0184775c696161
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-07-05 09:20:57 +0200

    Fix running into a blocking job

--- src/main/java/eu/dissco/nusearch/configuration/ApplicationConfiguration.java
+++ src/main/java/eu/dissco/nusearch/configuration/ApplicationConfiguration.java
@@ -5,19 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import eu.dissco.nusearch.component.DateDeserializer;
 import eu.dissco.nusearch.component.DateSerializer;
+import eu.dissco.nusearch.property.ApplicationProperties;
 import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.gbif.nameparser.NameParserGBIF;
 import org.gbif.nameparser.NameParserGbifV1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfiguration {
 
   public static final String DATE_STRING = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
+  private final ApplicationProperties properties;
+
   @Bean
   public NameParserGbifV1 nameParserGbifV1() {
-    return new NameParserGbifV1(20000);
+    return new NameParserGbifV1(new NameParserGBIF(properties.getNameParserThreadTimeOut(), 0,
+        properties.getNameParserMaxThreads()));
   }
 
   @Bean
